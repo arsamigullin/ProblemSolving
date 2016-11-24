3f0f9157a5ca22a73da21ead2665c6bc023b7ed9
albert samigullin <ar.smglln@gmail.com>
2016-11-24 16:23:27 +0300

    Implementing Stack Interface

---
+++ Algorithms/LinkedList/SinglyLinkedList/ImpementingStackInterface.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Attributes;
+using Algorithms.LinkedList.SinglyLinkedList.SinglyCellDescription;
+
+namespace Algorithms.LinkedList.SinglyLinkedList
+{
+    [DisplayInfo("LinkedListSingly", "Implementing Stack Interface", typeof(List<string>))]
+    public class ImpementingStackInterface
+    {
+        public List<string> Stack()
+        {
+            StackFromLinkedList<int> stackLinkedList = new StackFromLinkedList<int>();
+            stackLinkedList.Push(4);
+            stackLinkedList.Push(5);
+            stackLinkedList.Push(6);
+
+            var popres = stackLinkedList.Pop();
+            popres = stackLinkedList.Pop();
+            popres = stackLinkedList.Pop();
+            return new List<string>();
+        }
+    }
+    // Custom Stack Interface
+    public interface StackInterface<T>
+    {
+        void Push(T value);
+        T Pop();
+    }
+
+    // Custom Stack interface implementation
+    // FIFO
+    public class StackFromLinkedList<T> : StackInterface<T>
+    {
+        private ISinglyCell<T> initialCell;
+
+        // First input
+        public void Push(T value)
+        {
+            if (initialCell == null)
+            {
+                initialCell = new SinglyCell<T>();
+                initialCell.Value = value;
+                return;
+            }
+            ISinglyCell<T> newCell = new SinglyCell<T>();
+            newCell.Value = value;
+            newCell.Next = initialCell;
+            initialCell = newCell;
+        }
+        // Last output
+        public T Pop()
+        {
+            if (initialCell == null)
+            {
+                return default(T);
+            }
+            ISinglyCell<T> popedCell = initialCell;
+            initialCell = popedCell.Next;
+            return popedCell.Value;
+        }
+    }
+
+}
---
+++ Algorithms/LinkedList/SinglyLinkedList/ImplementingQueueInterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Attributes;
+
+namespace Algorithms.LinkedList.SinglyLinkedList
+{
+    [DisplayInfo("LinkedListSingly", "Implementing Queue Interface - not implemented yet", typeof(List<string>))]
+    public class ImplementingQueueInterface
+    {
+        //ISinglyCell<T> pushedCell = initialCell;
+        //while (pushedCell.Next != null)
+        //{
+        //    pushedCell = pushedCell.Next;
+        //}
+        //pushedCell.Next = new SinglyCell<T>();
+        //pushedCell.Next.Value = value;
+
+        //ISinglyCell<T> preLast=null;
+        //if (popedCell.Next == null)
+        //{
+        //    T value = popedCell.Value;
+        //    initialCell = null;
+        //    return value;
+        //}
+        //while (popedCell.Next!=null)
+        //{
+        //    preLast = popedCell;
+        //    popedCell = popedCell.Next;
+        //}
+        //preLast.Next = null;
+    }
+}
