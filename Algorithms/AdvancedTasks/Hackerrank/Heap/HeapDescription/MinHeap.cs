ec8cd255659e495f40b68c9738dd8443b755792c
Albert Samigullin <albert.samigullin@akvelon.com>
2017-01-13 19:22:25 +0300

    Tries

---
+++ Algorithms/AdvancedTasks/Hackerrank/Heap/HeapDescription/MinHeap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.AdvancedTasks.Hackerrank.Heap.HeapDescription
+{
+    class MinHeap : Heap
+    {
+        public MinHeap(int capacity) : base(capacity)
+        {
+
+        }
+        public override void heapifyDown()
+        {
+            int index = 0;
+            while (hasLeftChild(index))
+            {
+                int smallerChildIndex = getLeftChildIndex(index);
+
+                if (hasRightChild(index)
+                    && rightChild(index) < leftChild(index)
+                )
+                {
+                    smallerChildIndex = getRightChildIndex(index);
+                }
+
+                if (items[index] < items[smallerChildIndex])
+                {
+                    break;
+                }
+                else
+                {
+                    swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+            }
+        }
+
+        public override void heapifyUp()
+        {
+            int index = size - 1;
+
+            while (hasParent(index)
+                   && parent(index) > items[index]
+            )
+            {
+                swap(getParentIndex(index), index);
+                index = getParentIndex(index);
+            }
+        }
+    }
+}
---
+++ Algorithms/Trees/Tries/TrieDescription/TrieNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Trees.Tries.TrieDescription
+{
+    public class TrieNode
+    {
+        public char Value { get; set; }
+        public List<TrieNode> Children { get; set; }
+        public TrieNode Parent { get; set; }
+        public int Depth { get; set; }
+
+        public bool isCompletedWord { get; set; }
+        public int Count { get; set; }
+
+        public TrieNode(char value, int depth, TrieNode parent)
+        {
+            Value = value;
+            Children = new List<TrieNode>();
+            Depth = depth;
+            Parent = parent;
+        }
+
+        public bool IsLeaf()
+        {
+            return Children.Count == 0;
+        }
+
+        public TrieNode FindChildNode(char c)
+        {
+            foreach (var child in Children)
+                if (child.Value == c)
+                    return child;
+
+            return null;
+        }
+
+        public void DeleteChildNode(char c)
+        {
+            for (var i = 0; i < Children.Count; i++)
+                if (Children[i].Value == c)
+                    Children.RemoveAt(i);
+        }
+    }
+}
